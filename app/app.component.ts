3d85bdfb271705c97a0617c9310861ca16f1c293
J. Cohen <hostelmarketing@gmail.com>
2015-12-27 23:40:43 -0800

    Fix formatting. Add footer.

--- app/app.component.ts
+++ app/app.component.ts
@@ -27,21 +27,21 @@ interface Mutant {
         </div>
     `,
     styles:[`
-      .mutants {list-style-type: none; margin-left: 1em; padding: 0; width: 15em;}
-      .mutants li { cursor: pointer; }
-      .mutants li:hover {color: #369; background-color: #EEE; }
-      .mutants .badge {
-        font-size: small;
-        color: white;
-        padding: 0.1em 0.7em;
-        background-color: #369;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -1px;
-      }
-      .selected { background-color: #EEE; color: #369; }
-      .selected .badge { background-color: #333; }
+        .mutants {list-style-type: none; margin-left: 1em; padding: 0; width: 15em;}
+        .mutants li { cursor: pointer; }
+        .mutants li:hover {color: #369; background-color: #EEE; }
+        .mutants .badge {
+            font-size: small;
+            color: white;
+            padding: 0.1em 0.7em;
+            background-color: #369;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -1px;
+        }
+        .selected { background-color: #EEE; color: #369; }
+        .selected .badge { background-color: #333; }
     `]
 })
 
