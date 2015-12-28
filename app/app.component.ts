0a5943cf7d66a64e060df52d434b6edc1706e41a
J. Cohen <hostelmarketing@gmail.com>
2015-12-27 19:51:58 -0800

    Implement input with one-way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Saluton, Mondo!</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}}</h2>
+        <div><label>ID: </label>{{hero.id}}</div>
+        <div>
+            <label>Name: </label>
+            <div><input value="{{hero.name}}" placeholder="Name&hellip;"></div>
+        </div>
+    `
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public hero: Hero = {
+        id: 1,
+        name: 'Aardvark Dog'
+    };
+    public title = 'Superhero Database';
+}
